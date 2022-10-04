d5242d0795cf414964ab0389706c876015ce3812
ivandmitrienko <oneblackcrystal@gmail.com>
2022-10-04 22:36:30 +0300

    initial commit

--- shop/src/components/Layout.jsx
+++ shop/src/components/Layout.jsx
@@ -1,20 +1,34 @@
 import React, { PureComponent } from 'react';
 import { Outlet } from "react-router-dom";
 import { BsFillCartPlusFill } from 'react-icons/bs';
+import ShopIcons from './ShopIcons';
 import style from './Layout.module.css';
 
 
 export default class Layout extends PureComponent {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            displayIcons:false,
+        }
+    }
+
+    showDisplayIcons=()=>{
+        this.setState({displayIcons:!this.state.displayIcons})
+    };
+
     render() {
         return (
             <div className={style.Layout}>
                 <div className={style.menu}>
                     <h1>
-                       Add product to your cart list
+                        Add product to your cart list
                     </h1>
                     <div className={style.text}>
-                        <input type="text" placeholder='Product name'/>
-                        <input type="text" placeholder='Product price'/>
+                        <input type="text" placeholder='Product name' />
+                        <input type="text" placeholder='Product price' />
                     </div>
                     <div className={style.quantity}>
                         <button>+</button>
@@ -22,8 +36,11 @@ export default class Layout extends PureComponent {
                         <button>-</button>
                     </div>
                     <div className={style.addProduct}>
-                        <BsFillCartPlusFill size={40} color='rgb(173, 173, 173)'/>
+                        <BsFillCartPlusFill size={40} color='rgb(173, 173, 173)' onClick={this.showDisplayIcons}/>
                     </div>
+                    <>
+                    {this.state.displayIcons&&<ShopIcons />}
+                    </>
                     <div className={style.addToList}>
                         <button>Add to List</button>
                     </div>
---
+++ shop/src/components/ShopIcons.jsx
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react';
+import { images } from '../images.js';
+import style from './ShopIcons.module.css';
+
+export default class ShopIcons extends PureComponent {
+    render() {
+        return (
+            <div className={style.icons}>
+                {images.map((image) => <img src={image} key={image} alt=''/>)}
+            </div>
+        )
+    }
+}
