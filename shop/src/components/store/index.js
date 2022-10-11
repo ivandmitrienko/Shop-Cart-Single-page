7578c7d179359b10c8f5751b76ee4286e2e2a856
ivandmitrienko <oneblackcrystal@gmail.com>
2022-10-11 22:55:52 +0300

    initial commit

--- shop/src/components/store/index.js
+++ shop/src/components/store/index.js
@@ -1,5 +1,5 @@
 import { createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import reducers from '../reducers';
+// import { a } from '../reducers/Combinereducer';
 
-export default createStore(reducers, composeWithDevTools());
+export default createStore(reducer, composeWithDevTools());
