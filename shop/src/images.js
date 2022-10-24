8a324e8d5c6bc6fd3d7a9084bd0417e9b6f3b475
ivandmitrienko <oneblackcrystal@gmail.com>
2022-10-24 16:23:43 +0300

    initial commit

--- shop/src/images.js
+++ shop/src/images.js
@@ -1,1 +1,1 @@
-export const images = ['icons/apple-svgrepo-com.svg', 'icons/banana-svgrepo-com (1).svg', 'icons/beer-svgrepo-com.svg', 'icons/carrot-svgrepo-com.svg', 'icons/milk-svgrepo-com.svg'];
+export const images = ['/icons/apple-svgrepo-com.svg', '/icons/banana-svgrepo-com (1).svg', '/icons/beer-svgrepo-com.svg', '/icons/carrot-svgrepo-com.svg', '/icons/milk-svgrepo-com.svg'];
